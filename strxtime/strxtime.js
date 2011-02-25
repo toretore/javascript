71be3f12e7db6ebba7319193246bec4ff83dafa5
Tore Darell <toredarell@gmail.com>
2011-02-25 03:18:21 +0100

    Add tests, fix bugs

--- strxtime/strxtime.js
+++ strxtime/strxtime.js
@@ -1,12 +1,11 @@
 Strxtime = (function(){
 
   function zeroPad(num,pos){
-    pos=pos?pos:2;
-    var str=(pos==3 && num<100)?'0':'';
+    pos = pos ? pos : 2;
+    var str = (pos == 3 && num < 100 ) ? '0' : '';
     return str+(num < 10 ? '0'+num : ''+num);
-    
   };
-  
+
   function spacePad(num){
     return num < 10 ? ' '+num : ''+num;
   };
@@ -42,9 +41,6 @@ Strxtime = (function(){
         meridian: {am: 'AM', pm: 'PM'},
         weekdayNumbers:    [1, 2, 3, 4, 5, 6, 7], //By default, week starts on Sunday (which is stupid)
         weekdayNumbersMon: [7, 1, 2, 3, 4, 5, 6], // Monday is 1 but Sunday is 7 not zero
-        // the following magic regex's are borrowed from http://blog.stevenlevithan.com/archives/date-time-format
-        timezone: /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
-        timezoneClip: /[^-+\dA-Z]/g,
 
         '%': function(d){
           return '%';
@@ -65,20 +61,19 @@ Strxtime = (function(){
         'B': function(d){
           return this.monthNames[d.getMonth()];
         },
-        
+
         'c': function(d){ // The preferred date and time representation for the current locale.
           return d.toLocaleString();
         },
 
         'C': function(d){ // The century number (year/100) as a 2-digit integer.
-          var s=d.getFullYear().toString();
-          return s.substring(0,s.length-2);
+          return zeroPad(Math.floor(d.getFullYear() / 100));
         },
 
         'd': function(d){
           return zeroPad(d.getDate());
         },
-        
+
         'D': function(d){ //%m/%d/%y
           return this.m(d)+'/'+this.d(d)+'/'+this.y(d);
         },
@@ -104,16 +99,17 @@ Strxtime = (function(){
           if (h == 0 || h == 12) return 12;
           else return zeroPad(h % 12);
         },
-        
+
         'j': function(d){ // The day of the year as a decimal number (range 001 to 366).
-          var jan1=new Date(d.getFullYear(),0,1,0,0,0,0);
-          return ""+Math.floor(1+(d.getTime()-jan1.getTime())/86400000);
+          var jan1 = Date.UTC(d.getFullYear(), 0, 1, 0, 0, 0, 0),
+              thatday = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), 0, 0, 0, 0);
+          return zeroPad(((thatday - jan1) / 86400000) + 1, 3);
         },
 
-        'k': function(d){ // The hour (24-hour clock) as a decimal number (range 0 to 23); single digits are preceded by a blank. (See also %H.) 
+        'k': function(d){ // The hour (24-hour clock) as a decimal number (range 0 to 23); single digits are preceded by a blank. (See also %H.)
           return spacePad(d.getHours());
         },
-        
+
         'l': function(d){ // The hour (12-hour clock) as a decimal number (range 1 to 12); single digits are preceded by a blank. (See also %I.)
           var h = d.getHours();
           if (h == 0 || h == 12) return 12;
@@ -137,7 +133,7 @@ Strxtime = (function(){
           return h < 12 ? this.meridian.am : this.meridian.pm;
         },
 
-        'P': function(d){ // Like %p but in lowercase: `am' or `pm' or a corresponding string for the current locale. 
+        'P': function(d){ // Like %p but in lowercase: `am' or `pm' or a corresponding string for the current locale.
           return this.p(d).toLowerCase();
         },
 
@@ -150,30 +146,30 @@ Strxtime = (function(){
         },
 
         's': function(d){ // seconds since epoch
-          var s=d.getTime().toString();
+          var s = d.getTime().toString();
           return s.substring(0,s.length-3);
+        },
+
+        'S': function(d){
+          return zeroPad(d.getSeconds());
         },
 
         't': function(d){ // a tab character
           return "\t";
         },
 
-        'S': function(d){
-          return zeroPad(d.getSeconds());
-        },
-        
         'T': function(d){ // %H:%M:%S
           return this.H(d)+':'+this.M(d)+':'+this.S(d);
         },
-        
-        'u': function(d){ // The day of the week as a decimal, range 1 to 7, Monday being 1. See also %w. 
+
+        'u': function(d){ // The day of the week as a decimal, range 1 to 7, Monday being 1. See also %w.
           return ''+this.weekdayNumbersMon[d.getDay()];
         },
 
         'w': function(d){
           return ''+this.weekdayNumbers[d.getDay()];
         },
-        
+
         'x': function(d){ // The preferred date representation for the current locale without the time.
           return d.toLocaleDateString();
         },
@@ -190,20 +186,10 @@ Strxtime = (function(){
           return ''+d.getFullYear();
         },
 
-        'z': function(d){ // The time-zone as hour offset from GMT. Required to emit RFC822-conformant dates (using "%a, %d %b %Y %H:%M:%S %z").
-          var minutes=Math.abs(d.getTimezoneOffset());
-          var sign=(d.getTimezoneOffset()>0)?'-':'+';
-          return sign+zeroPad(Math.floor(minutes/60))+""+zeroPad(minutes%60);
-        },
-        
-        'Z': function(d){
-          // use the magic regex's from http://blog.stevenlevithan.com/archives/date-time-format
-          return (String(d).match(this.timezone) || [""]).pop().replace(this.timezoneClip, "");
-        },
-        
-        '+': function(d){ // The date and time in date(1) format. 
-          //%a %b %e %H:%M:%S %Z %Y by my estimation
-          return this.a(d)+" "+this.b(d)+" "+this.e(d)+" "+this.H(d)+":"+this.M(d)+":"+this.S(d)+" "+this.Z(d)+" "+this.Y(d);
+        'z': function(d){ // The time-zone as hour offset from UTC. Required to emit RFC822-conformant dates (using "%a, %d %b %Y %H:%M:%S %z").
+          var minutes = Math.abs(d.getTimezoneOffset());
+          var sign = (d.getTimezoneOffset() > 0) ? '-' : '+';
+          return sign + zeroPad(Math.floor(minutes/60)) + "" + zeroPad(minutes % 60);
         }
 
       }
@@ -212,13 +198,13 @@ Strxtime = (function(){
 
     strftime: function(date, str, locale){
       var formats = this.formats[locale || this.defaultLocale] || {};
-      return str.replace(/%([a-zA-Z%+])/g, function(s, f){
+      return str.replace(/%([a-zA-Z%])/g, function(s, f){
         return formats[f] ? formats[f](date) : s;
       });
     },
 
     strptime: function(){
-      
+
     }
 
   };
--- strxtime/test/strxtime_test.js
+++ strxtime/test/strxtime_test.js
@@ -11,12 +11,12 @@ StrftimeTest = TestCase("StrftimeTest", {
   setUp: function(){
     this.d = new Date(2010, 10, 12, 13, 14, 15); //13:14:15 Friday Nov 12, 2010
   },
-  
+
 
   'test %% should be replaced with a single %': function(){
     assertEquals('umma % gumma 10% of all people like carrots', X.strftime(this.d, 'umma %% gumma 10%% of all people like carrots'));
   },
-  
+
   'test non-existing formats should be left in place': function(){
     assertEquals('i %g like %f turtles', X.strftime(this.d, 'i %g like %f turtles'));
   },
@@ -45,10 +45,46 @@ StrftimeTest = TestCase("StrftimeTest", {
     assertEquals('Saturday June 12', X.strftime(this.d, 'Saturday %B 12'));
   },
 
+  'test %c should be replaced with whatever toLocaleString returns': function(){
+    assertEquals('Preferred locale: '+this.d.toLocaleString(), X.strftime(this.d, 'Preferred locale: %c'));
+  },
+
+  'test %C should return two-digit century': function(){
+    assertEquals('20th century', X.strftime(this.d, '%Cth century'));
+    this.d.setFullYear(450);
+    assertEquals('04th century', X.strftime(this.d, '%Cth century'));
+    this.d.setFullYear(50);
+    assertEquals('00th century', X.strftime(this.d, '%Cth century'));
+  },
+
   'test %d should be replaced with zero-padded day of the month': function(){
     assertEquals('Friday November 12', X.strftime(this.d, 'Friday November %d'));
     this.d.setDate(9);
     assertEquals('Tuesday November 09', X.strftime(this.d, 'Tuesday November %d'));
+  },
+
+  'test %D should be replaced with the same as %m/%d/%y': function(){
+    assertEquals(X.strftime(this.d, 'umma %m/%d/%y gumma'), X.strftime(this.d, 'umma %D gumma'));
+  },
+
+  'test %e should be replaced with space-padded date of the month': function(){
+    assertEquals('Fri Nov 12', X.strftime(this.d, 'Fri Nov %e'));
+    this.d.setDate(3);
+    assertEquals('Fri Nov  3', X.strftime(this.d, 'Fri Nov %e'));
+  },
+
+  'test %F should be replaced with the same as %Y-%m-%d': function(){
+    assertEquals(X.strftime(this.d, 'The date is %Y-%m-%d'), X.strftime(this.d, 'The date is %F'));
+    this.d.setDate(1);
+    assertEquals(X.strftime(this.d, 'The date is %Y-%m-%d'), X.strftime(this.d, 'The date is %F'));
+    this.d.setYear(45);
+    assertEquals(X.strftime(this.d, 'The date is %Y-%m-%d'), X.strftime(this.d, 'The date is %F'));
+  },
+
+  'test %h should be replaced with the same as %b': function(){
+    assertEquals(X.strftime(this.d, 'Friday %b 12'), X.strftime(this.d, 'Friday %h 12'));
+    this.d.setMonth(5);
+    assertEquals(X.strftime(this.d, 'Saturday %b 12'), X.strftime(this.d, 'Saturday %h 12'));
   },
 
   'test %H should be replaced with 24-hour zero-padded hour of the day': function(){
@@ -75,10 +111,42 @@ StrftimeTest = TestCase("StrftimeTest", {
     assertEquals('time is 12:14:15 pm right now', X.strftime(this.d, 'time is %I:14:15 pm right now'));
   },
 
-  //TODO Skipping this for now, because Date doesn't have a getter for day of year
-  //'test %j should be replaced with zero-padded day of the year': function(){
-  //  assertEquals('this is the th day of the year', this.d, 'this is the %jth day of the year');
-  //}
+  'test %j should be replaced with zero-padded day of the year': function(){
+    var d = new Date(2010, 0, 1);
+    assertEquals('this is the 001st day of the year', X.strftime(d, 'this is the %jst day of the year'));
+    d.setDate(20);
+    assertEquals('this is the 020th day of the year', X.strftime(d, 'this is the %jth day of the year'));
+    d.setDate(40);
+    assertEquals('this is the 040th day of the year', X.strftime(d, 'this is the %jth day of the year'));
+    d.setDate(1);
+    d.setMonth(0);
+    d.setDate(234);
+    assertEquals('this is the 234th day of the year', X.strftime(d, 'this is the %jth day of the year'));
+  },
+
+  'test %k should be replaced with 24-hour space-padded hour of the day': function(){
+    assertEquals('time is 13:14:15 right now', X.strftime(this.d, 'time is %k:14:15 right now'));
+    this.d.setHours(5);
+    assertEquals('time is  5:14:15 right now', X.strftime(this.d, 'time is %k:14:15 right now'));
+    this.d.setHours(23);
+    assertEquals('time is 23:14:15 right now', X.strftime(this.d, 'time is %k:14:15 right now'));
+    this.d.setHours(0);
+    assertEquals('time is  0:14:15 right now', X.strftime(this.d, 'time is %k:14:15 right now'));
+  },
+
+  'test %l should be replaced with 12-hour space-padded hour of the day': function(){
+    assertEquals('time is  1:14:15 pm right now', X.strftime(this.d, 'time is %l:14:15 pm right now'));
+    this.d.setHours(5);
+    assertEquals('time is  5:14:15 am right now', X.strftime(this.d, 'time is %l:14:15 am right now'));
+    this.d.setHours(23);
+    assertEquals('time is 11:14:15 pm right now', X.strftime(this.d, 'time is %l:14:15 pm right now'));
+
+    //Stupid 12 hour clock exceptions
+    this.d.setHours(0);
+    assertEquals('time is 12:14:15 am right now', X.strftime(this.d, 'time is %l:14:15 am right now'));
+    this.d.setHours(12);
+    assertEquals('time is 12:14:15 pm right now', X.strftime(this.d, 'time is %l:14:15 pm right now'));
+  },
 
   'test %m should be replaced with zero-padded 1-indexed month number of the year': function(){
     assertEquals('November is the 11th month of the year', X.strftime(this.d, 'November is the %mth month of the year'));
@@ -100,6 +168,10 @@ StrftimeTest = TestCase("StrftimeTest", {
     assertEquals('time is 13:00:15 right now', X.strftime(this.d, 'time is 13:%M:15 right now'));
   },
 
+  '%n should be replaced with a newline': function(){
+    assertEquals('time:\n13:14', X.strftime(this.d, 'time:%n%H:%M'));
+  },
+
   'test %p should be replaced by meridian indicator': function(){
     assertEquals('time is 01:14:15 PM right now', X.strftime(this.d, 'time is 01:14:15 %p right now'));
     this.d.setHours(5);
@@ -109,9 +181,37 @@ StrftimeTest = TestCase("StrftimeTest", {
 
     //Stupid 12 hour clock exceptions
     this.d.setHours(0);
-    assertEquals('time is 12:14:15 AM right now', X.strftime(this.d, 'time is 12:14:15 AM right now'));
+    assertEquals('time is 12:14:15 AM right now', X.strftime(this.d, 'time is 12:14:15 %p right now'));
     this.d.setHours(12);
-    assertEquals('time is 12:14:15 PM right now', X.strftime(this.d, 'time is 12:14:15 PM right now'));
+    assertEquals('time is 12:14:15 PM right now', X.strftime(this.d, 'time is 12:14:15 %p right now'));
+  },
+
+  'test %P should be replaced with lowercase meridian indicator': function(){
+    assertEquals('time is 01:14:15 pm right now', X.strftime(this.d, 'time is 01:14:15 %P right now'));
+    this.d.setHours(5);
+    assertEquals('time is 05:14:15 am right now', X.strftime(this.d, 'time is 05:14:15 %P right now'));
+    this.d.setHours(23);
+    assertEquals('time is 11:14:15 pm right now', X.strftime(this.d, 'time is 11:14:15 %P right now'));
+
+    //Stupid 12 hour clock exceptions
+    this.d.setHours(0);
+    assertEquals('time is 12:14:15 am right now', X.strftime(this.d, 'time is 12:14:15 %P right now'));
+    this.d.setHours(12);
+    assertEquals('time is 12:14:15 pm right now', X.strftime(this.d, 'time is 12:14:15 %P right now'));
+  },
+
+  'test %r should be replaced with the same as %I:%M:%S %p': function(){
+    assertEquals(X.strftime(this.d, '%I:%M:%S %p'), X.strftime(this.d, '%r'));
+  },
+
+  'test %R should be replaced with the same as %H:%M': function(){
+    assertEquals(X.strftime(this.d, '%H:%M'), X.strftime(this.d, '%R'));
+  },
+
+  'test %s should be replaced with seconds since epoch, UTC': function(){
+    assertEquals('1289564055 seconds since epoch', X.strftime(this.d, '%s seconds since epoch'));
+    this.d.setHours(14);
+    assertEquals('1289567655 seconds since epoch', X.strftime(this.d, '%s seconds since epoch'));
   },
 
   'test %S should be replaced with zero-padded second of the minute': function(){
@@ -124,12 +224,38 @@ StrftimeTest = TestCase("StrftimeTest", {
     assertEquals('time is 13:14:00 right now', X.strftime(this.d, 'time is 13:14:%S right now'));
   },
 
+  'test %t should be replaced with a tab': function(){
+    assertEquals('2010\t11\t12', X.strftime(this.d, '%Y%t%m%t%d'));
+  },
+
+  'test %T should be replaced with the same as %H:%M:%S': function (){
+    assertEquals(X.strftime(this.d, '%H:%M:%S on a Friday'), X.strftime(this.d, '%T on a Friday'));
+  },
+
+  'test %u should be replaced with week day number, Monday being 1, Sunday 7': function(){
+    assertEquals('this is the 5th day of the week', X.strftime(this.d, 'this is the %uth day of the week'));
+    this.d.setDate(13);//Sat nov 13
+    assertEquals('this is the 6th day of the week', X.strftime(this.d, 'this is the %uth day of the week'));
+    this.d.setDate(7);//Sun nov 7
+    assertEquals('this is the 7th day of the week', X.strftime(this.d, 'this is the %uth day of the week'));
+    this.d.setDate(8);//Mon nov 8
+    assertEquals('this is the 1st day of the week', X.strftime(this.d, 'this is the %ust day of the week'));
+  },
+
   'test %w should be replaced with week day number, Sunday being 0': function(){
     assertEquals('this is the 6th day of the week', X.strftime(this.d, 'this is the %wth day of the week'));
     this.d.setDate(13);//Sat nov 13
     assertEquals('this is the 7th day of the week', X.strftime(this.d, 'this is the %wth day of the week'));
     this.d.setDate(7);//Sun nov 7
     assertEquals('this is the 1st day of the week', X.strftime(this.d, 'this is the %wst day of the week'));
+  },
+
+  'test %x should be replaced with whatever toLocaleDateString returns': function(){
+    assertEquals('a date: '+this.d.toLocaleDateString(), X.strftime(this.d, 'a date: %x'));
+  },
+
+  'test %X should be replaced with whatever toLocaleTimeString returns': function(){
+    assertEquals('a date: '+this.d.toLocaleTimeString(), X.strftime(this.d, 'a date: %X'));
   },
 
   'test %y should be replaced with zero-padded year without century': function(){
@@ -250,7 +376,7 @@ StrftimeLocaleTest = TestCase('StrftimeLocaleTest', {
     this.d.setHours(15);
     assertEquals('03 på ættmeddan', X.strftime(this.d, '%I %p', 'tr'));
   },
-  
+
   'test should be able to override weekday numbers': function(){
     X.addLocale('not stupid', {weekdayNumbers:[7,1,2,3,4,5,6]}, X.formats['default']);
     assertEquals('Friday is the 5th day of the week', X.strftime(this.d, '%A is the %wth day of the week', 'not stupid'));
